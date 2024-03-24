2a06d2cd6a8c125be27572f818025eb48aef4d57
David Marin Sanchez <dmarin@theknotww.com>
2024-03-24 20:26:06 +0100

    extract constant

--- src/lib/Infrastructure/Auth/strategies/jwt.strategy.ts
+++ src/lib/Infrastructure/Auth/strategies/jwt.strategy.ts
@@ -2,18 +2,23 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { Roles } from 'src/lib/Domain/constants/roles';
+
+export const SECRET_KEY = 'monsters-api';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process,
+      secretOrKey: SECRET_KEY,
     });
   }
 
   async validate(payload: any) {
+    if (!payload.role) {
+      throw new UnauthorizedException();
+    }
+
     return payload;
   }
 }
